0b1c8f649eca3ece8733c62aea9bbe6f846a0420
Crim <stephen.powis@gmail.com>
2019-12-02 16:45:33 +0900

    Cleanup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,6 @@
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## 0.0.1 Initial Release
+## 0.1.0 Initial Release
 
 Initial release.  Should not be considered stable.
--- src/main/java/org/sourcelab/http/rest/RestClient.java
+++ src/main/java/org/sourcelab/http/rest/RestClient.java
@@ -14,8 +14,8 @@
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
  * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package org.sourcelab.http.rest;
 
+package org.sourcelab.http.rest;
 
 import org.sourcelab.http.rest.configuration.Configuration;
 import org.sourcelab.http.rest.request.Request;
